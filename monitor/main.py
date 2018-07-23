8e458dd15fe462b2a370cf6b7b34e58bb0e61e99
mohammad RaoofNia <mraoofnia@gmail.com>
2018-07-23 04:56:28 +0430

    codes of 1397-4-31
    windows compiler and some code

---
+++ monitor/main.py
@@ -0,0 +1,13 @@
+import os
+
+def get_size(start_path = '.'):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            total_size += os.path.getsize(fp)
+    return total_size
+
+def sizes(directory):
+    for d in os.listdir(directory):
+	    print(d," size is : ", get_size(directory + d))
